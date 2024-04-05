9a1ae8daca402644ca542bb91709dc3a9f5eea6f
AkankshaMishra11 <mishraakanksha1309@gmail.com>
2024-04-05 21:05:47 +0530

    coding assessment, report generation, result compilation

--- lib/screens/pythontutorial.dart
+++ lib/screens/pythontutorial.dart
@@ -16,6 +16,11 @@ import 'package:stela_app/screens/SklearnexperimentList copy.dart';
 import 'package:stela_app/screens/KerasexperimentList copy.dart';
 import 'package:stela_app/screens/PytorchexperimentList copy.dart';
 import 'package:stela_app/screens/AIPTassessmentPage.dart';
+import 'package:stela_app/screens/CodingAssessmentexperimentList.dart';
+import 'package:stela_app/screens/CodingAssessmentexperimentList.dart';
+import 'package:stela_app/screens/AIPTcodingTestResults.dart';
+import 'package:stela_app/screens/AIPTmcqTestResults.dart';
+
 
 var expNum;
 
@@ -691,15 +696,263 @@ Container(
                                                                               )),
                                                                           onPressed:
                                                                               () {
+                                                                                showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      String enteredPin = ''; // Variable to store the entered pin
+      return AlertDialog(
+        title: Text('Enter Pin'),
+        content: TextField(
+          onChanged: (value) {
+            enteredPin = value; // Update entered pin as the user types
+          },
+          obscureText: true, // Hide entered pin characters
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            hintText: 'Enter 6-digit Pin',
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Submit'),
+            onPressed: () {
+              // Predefined 6-digit pin for comparison
+              String predefinedPin = '142615';
+
+              // Check if the entered pin matches the predefined pin
+              if (enteredPin == predefinedPin) {
+                // Navigate to the desired page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AIPTAssessmentPage()),
+                );
+                 // Add this variable to track whether the link has been clicked
+
+
+              } else {
+                // Show an error message if the pin is incorrect
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Incorrect Pin. Please try again.'),
+                  ),
+                );
+              }
+            },
+                                                                            
+                                                                         
+                                                                        ),
+                                                                         ],
+      );
+    },
+  );
+                                                                          },
+                                                                        ),
+                                                                        Container(
+                                      child: Column(
+                                        children: [
+                                          Container(
+                                            width: 50,
+                                            margin: EdgeInsets.all(10),
+                                            child: ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(150),
+                                              /*child: Image(
+                                          image: NetworkImage(
+                                              'https://w7.pngwing.com/pngs/827/120/png-transparent-educational-assessment-test-computer-icons-risk-assessment-assess-angle-text-logo-thumbnail.png'),
+                                        ),*/
+                                            ),
+                                          ),
+TextButton(
+                                                                          child: Container(
+                                                                              width: double.infinity,
+                                                                              padding: EdgeInsets.symmetric(vertical: 5),
+                                                                              decoration: BoxDecoration(
+                                                                                color: primaryButton,
+                                                                                borderRadius: BorderRadius.circular(10),
+                                                                                border: Border.all(width: 2.0, color: primaryBar),
+                                                                              ),
+                                                                              child: Text(
+                                                                                'Coding Assessment',
+                                                                                style: TextStyle(
+                                                                                  //color: Colors.white,
+                                                                                  fontSize: 15, fontFamily: 'PTSerif-Bold', fontWeight: FontWeight.bold,
+                                                                                  color: primaryBar,
+                                                                                ),
+                                                                                textAlign: TextAlign.center,
+                                                                              )),
+                                                                          /*onPressed:
+                                                                              () {
+                                                                                showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      String enteredPin = ''; // Variable to store the entered pin
+      return AlertDialog(
+        title: Text('Enter Pin'),
+        content: TextField(
+          onChanged: (value) {
+            enteredPin = value; // Update entered pin as the user types
+          },
+          obscureText: true, // Hide entered pin characters
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            hintText: 'Enter 6-digit Pin',
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Submit'),
+            onPressed: () {
+              // Predefined 6-digit pin for comparison
+              String predefinedPin = '789789';
+
+              // Check if the entered pin matches the predefined pin
+              if (enteredPin == predefinedPin) {
+                // Navigate to the desired page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CodingAssessmentExperimentList()),
+                );
+                 // Add this variable to track whether the link has been clicked
+                /*predefinedPin = '';
+
+              } else {
+                // Show an error message if the pin is incorrect
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Incorrect Pin. Please try again.'),
+                  ),
+                );
+              }
+            },*/
+                                                                            
+                                                                         
+                                                                        ),
+
+                                                                        
+                                                                        
+                                                                         ],
+      );
+    },
+  );
+                                                                          },
+                                                                        
+                                                                            
+                                                                          
+                                                                        ),*/
+                                                                         onPressed:
+                                                                              () {
                                                                             Navigator.push(
                                                                               context,
-                                                                              MaterialPageRoute(builder: (context) => AIPTAssessmentPage()),
+                                                                              MaterialPageRoute(builder: (context) => CodingAssessmentExperimentList()),
                                                                             );
                                                                           },
                                                                         ),
+                                                                        
+                                                                        Container(
+                                                                    child:
+                                                                        Column(
+                                                                      children: [
+                                                                        Container(
+                                                                          width:
+                                                                              50,
+                                                                          margin:
+                                                                              EdgeInsets.all(10),
+                                                                          child:
+                                                                              ClipRRect(
+                                                                            borderRadius:
+                                                                                BorderRadius.circular(150),
+                                                                            /*child: Image(
+                                          image: NetworkImage(
+                                              'https://w7.pngwing.com/pngs/827/120/png-transparent-educational-assessment-test-computer-icons-risk-assessment-assess-angle-text-logo-thumbnail.png'),
+                                        ),*/
+                                                                          ),
+                                                                        ),
+                                                                        TextButton(
+                                                                          child: Container(
+                                                                              width: double.infinity,
+                                                                              padding: EdgeInsets.symmetric(vertical: 5),
+                                                                              decoration: BoxDecoration(
+                                                                                color: primaryButton,
+                                                                                borderRadius: BorderRadius.circular(10),
+                                                                                border: Border.all(width: 2.0, color: primaryBar),
+                                                                              ),
+                                                                              child: Text(
+                                                                                'Coding Assessment Results',
+                                                                                style: TextStyle(
+                                                                                  //color: Colors.white,
+                                                                                  fontSize: 15, fontFamily: 'PTSerif-Bold', fontWeight: FontWeight.bold,
+                                                                                  color: primaryBar,
+                                                                                ),
+                                                                                textAlign: TextAlign.center,
+                                                                              )),
+                                                                          onPressed:
+                                                                              () {
+                                                                            Navigator.push(
+                                                                              context,
+                                                                              MaterialPageRoute(builder: (context) => TablePage()),
+                                                                            );
+                                                                          },
+                                                                        ),
+],
+                                                                    ),
+                                                                  ),
 
 
+                                                                  /*Container(
+                                                                    child:
+                                                                        Column(
+                                                                      children: [
+                                                                        Container(
+                                                                          width:
+                                                                              50,
+                                                                          margin:
+                                                                              EdgeInsets.all(10),
+                                                                          child:
+                                                                              ClipRRect(
+                                                                            borderRadius:
+                                                                                BorderRadius.circular(150),
+                                                                            /*child: Image(
+                                          image: NetworkImage(
+                                              'https://w7.pngwing.com/pngs/827/120/png-transparent-educational-assessment-test-computer-icons-risk-assessment-assess-angle-text-logo-thumbnail.png'),
+                                        ),*/
+                                                                          ),
+                                                                        ),
+                                                                        TextButton(
+                                                                          child: Container(
+                                                                              width: double.infinity,
+                                                                              padding: EdgeInsets.symmetric(vertical: 5),
+                                                                              decoration: BoxDecoration(
+                                                                                color: primaryButton,
+                                                                                borderRadius: BorderRadius.circular(10),
+                                                                                border: Border.all(width: 2.0, color: primaryBar),
+                                                                              ),
+                                                                              child: Text(
+                                                                                'MCQ Assessment Results',
+                                                                                style: TextStyle(
+                                                                                  //color: Colors.white,
+                                                                                  fontSize: 15, fontFamily: 'PTSerif-Bold', fontWeight: FontWeight.bold,
+                                                                                  color: primaryBar,
+                                                                                ),
+                                                                                textAlign: TextAlign.center,
+                                                                              )),
+                                                                          onPressed:
+                                                                              () {
+                                                                            Navigator.push(
+                                                                              context,
+                                                                              MaterialPageRoute(builder: (context) => AIPTMCQTablePage()),
+                                                                            );
+                                                                          },
+                                                                        ),
+],
+                                                                    ),
+                                                                  ),*/
+
+
+                                                                  
                                                                       ],
+                                                                    ),
+                                                                  ),
+                                                                   ],
                                                                     ),
                                                                   ),
                                                                 ],
